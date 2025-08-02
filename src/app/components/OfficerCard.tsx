58a220f3dea648617daadbf6234a99ec33f26c12
Akhil Kasamsetty <awesomesetty99@gmail.com>
2025-08-02 13:13:10 -0500

    added /officers breakpoint window scaling

--- src/app/components/OfficerCard.tsx
+++ src/app/components/OfficerCard.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 
 export default function OfficerCard({ officer }: { officer: Officer }) {
     return (
-        <div id={`${officer.id}`} className="flex flex-col gap-5 p-11 border border-gray-300 shadow-md/30 shadow-gray-600 rounded-2xl">
+        <div id={`${officer.id}`} className="flex flex-col gap-3 sm:gap-5 p-6 sm:p-8 md:p-11 border border-gray-300 shadow-md/30 shadow-gray-600 rounded-2xl">
             {/* Image section w/ clickable button to open modal */}
             <Link href={`/officers/${officer.id}`}>
                 <Image src={`/officerImages/cropped/${officer.id}.JPG`} alt={`${officer.name}'s photo`} 
                     className="rounded-2xl hover:scale-105 duration-200 hover:brightness-110 transition-[transform, colors]" width={300} height={300}/>
-            </Link>
+            </Link> 
             {/* Text section with name, position, major, and year */}
-            <div className="flex flex-col gap-3">
-                <h3 className="font-primary tracking-widest text-3xl">{officer.name}</h3>
-                <p className="font-secondary text-xl">{officer.position}</p>
-                <p className="font-secondary text-xl">{officer.major}</p>
+            <div className="flex flex-col gap-1 sm:gap-2 md:gap-3">
+                <h3 className="font-primary tracking-widest text-base sm:text-2xl md:text-3xl">{officer.name}</h3>
+                <p className="font-secondary text-xs sm:text-lg md:text-xl">{officer.position}</p>
+                <p className="font-secondary text-xs sm:text-lg md:text-xl">{officer.major}</p>
             </div>
         </div>
     );
--- src/app/officers/page.tsx
+++ src/app/officers/page.tsx
@@ -4,12 +4,12 @@ import OfficerCard from "@/app/components/OfficerCard";
 
 const OfficerPage = () => {
     return (
-        <div className="flex flex-col items-center gap-5 md:gap-10 px-20 md:px-0">
+        <div className="flex flex-col items-center gap-5 md:gap-10 mx-7 sm:mx-15">
           {/* <header className="flex flex-col text-center mt-10 md:mt-15 gap-3">
             <h1 className="font-primary tracking-wider text-3xl md:text-4xl">Officers</h1>
           </header> */}
-          <main className="mt-10 md:mt-15 md:mb-15">
-            <div className='grid grid-cols-3 grid-rows-6 gap-12'>
+          <main className="mt-10 md:mt-15 mb-10 md:mb-15">
+            <div className='grid gap-5 grid-cols-2 sm:grid-rows-9 sm:gap-12 lg:grid-cols-3 lg:grid-rows-6 lg:gap-12'>
               {
                   OfficerList.map((officer) => (
                       <OfficerCard key={officer.name} officer={officer} />
