16d4884f8317561f96fd8016f091f185cbb26b86
Akhil Kasamsetty <awesomesetty99@gmail.com>
2025-08-02 04:31:37 -0500

    added internal navigation by giving each OfficerCard an id, added hash linking

--- src/app/officers/[imageId]/page.tsx
+++ src/app/officers/[imageId]/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import { RiArrowLeftCircleFill, RiArrowRightCircleFill, RiArrowLeftSLine, RiArrowRightSLine, RiDownloadLine, RiCloseLargeLine} from "react-icons/ri";
 
 export default async function ModalImage({ params }: { params: Promise<{imageId: number}> }) {
     const { imageId } = await params;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black">s
-            <Link href="/officers" className="absolute -inset-0 cursor-default z-0">
+        <div className="fixed inset-0 flex items-center justify-center bg-black z-20">
+            <Link href={`/officers#officer-${imageId}`} className="absolute -inset-0 cursor-default z-20">
                 <Image alt={`Officer ${imageId} Blurred Background`} src={`/officerImages/raw/${imageId}.JPG`} fill className="max-w-full max-h-screen blur-xl" />
             </Link>
-            <div className="bg-white p-0 rounded-lg shadow-lg z-10">
+            <div className="bg-white p-0 rounded-lg shadow-lg z-30">
                 <Image alt={`Officer ${imageId}`} src={`/officerImages/raw/${imageId}.JPG`} width={1000} height={1000} className="max-w-full max-h-screen w-auto h-150" />
             </div>
         </div>  
