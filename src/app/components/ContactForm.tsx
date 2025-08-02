5d9296f00167a094ccaeabe225129e154d2785fd
Akhil Kasamsetty <awesomesetty99@gmail.com>
2025-08-02 15:34:42 -0500

    connected labels & inputs for contact form

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -24,37 +24,41 @@ const ContactForm: FC = () => {
             Fill out this form below and we'll get back to you as soon as possible!
         </h2>   
         <div className='flex flex-col gap-1'>
-            <label htmlFor='name' className='text-base md:text-lg'>Name</label>
+            <label htmlFor='formname' className='text-base md:text-lg'>Name</label>
             <input
                 type="text"
                 placeholder="Your name"
+                id='formname'
                 {...register("name", { required: true })}
                 className="px-3 py-2 border rounded-lg border-gray-400 text-sm md:text-base"
             />
         </div>
         <div className='flex flex-col gap-1'>
-            <label htmlFor='email' className='text-base md:text-lg'>Email</label>
+            <label htmlFor='formemail' className='text-base md:text-lg'>Email</label>
             <input
                 type="email"
                 placeholder="example@domain.com"
+                id='formemail'
                 {...register("email", { required: true })}
                 className="px-3 py-2 border rounded-lg border-gray-400 text-sm md:text-base"
             />
         </div>
         <div className='flex flex-col gap-1'>
-            <label htmlFor='subject' className='text-base md:text-lg'>Subject</label>
+            <label htmlFor='formsubject' className='text-base md:text-lg'>Subject</label>
             <input
                 type="text"
                 placeholder="How can we help?"
+                id='formsubject'
                 {...register("subject", { required: true })}
                 className="px-3 py-2 border rounded-lg border-gray-400 text-sm md:text-base"
             />
         </div>
         <div className='flex flex-col gap-1'>
-            <label htmlFor='message' className='text-base md:text-lg'>Message</label>
+            <label htmlFor='formmessage' className='text-base md:text-lg'>Message</label>
             <textarea
                 rows={5}
                 placeholder="Type your message here..."
+                id='formmessage'
                 {...register("message", { required: true })}
                 className="px-3 py-2 border rounded-lg border-gray-400 h-32 text-sm md:text-base"
             />
--- src/app/components/ImageModal.tsx
+++ src/app/components/ImageModal.tsx
@@ -11,9 +11,18 @@ export default function ImageModal({ imageId, className }: ImageModalProps) {
     return (
         <div className={`relative ${className}`}>
             {/* Actual Image */}
-            <Image alt={`Officer ${imageId}`} src={`/officerImages/raw/${imageId}.JPG`} width={1000} height={1000} className="max-w-full max-h-screen w-auto h-150 rounded-2xl shadow-lg" />
-            {/* Buttons */}
-            <div className="absolute inset-3 mx-auto flex items-center justify-center">
+            {/* <div className="my-10 mx-10">
+                <Image alt={`Officer ${imageId}`} src={`/officerImages/raw/${imageId}.JPG`} width={1000} height={1000} 
+                    className="rounded-2xl shadow-lg" 
+                />
+            </div> */}
+            <div className="flex flex-col my-10 mx-10 rounded-2xl ">
+                <Image alt={`Officer ${imageId}`} src={`/officerImages/raw/${imageId}.JPG`} fill
+                    className="rounded-2xl shadow-lg" objectFit="contain" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+            </div>
+            {/* Buttons */} 
+            <div className="absolute inset-3 mx-10 my-10 flex items-center justify-center">
                 <Link href={`/officers#${imageId}`} className="absolute top-0 left-0 p-1 items-center bg-white/70 rounded-full hover:text-primary transition-colors">
                     <RiCloseLine className="w-8 h-8" />
                 </Link>
