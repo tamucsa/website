eace38c55a27be4fc98547411542df4e9f61543e
Akhil Kasamsetty <awesomesetty99@gmail.com>
2025-08-19 18:10:28 -0500

    fixed home page video scaling

--- src/app/(nav-pages)/page.tsx
+++ src/app/(nav-pages)/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
           {/* What is CSA? */}
           <div className="flex flex-col gap-3 lg:gap-5">
             <h2 className="text-2xl lg:text-4xl font-primary"><u>What is CSA?</u></h2>
-            <p className="text-base lg:text-xl">
-            The Texas A&M University Chinese Student Association (TAMU CSA) is a welcoming community dedicated to connecting students from all backgrounds through cultural, social, and educational experiences. No matter who you are, joining CSA is a great way to make new friends, explore Chinese culture, and get involved on campus. Attend our events and general meetings to discover opportunities for personal growth, leadership, and lifelong memories. We’re excited to help you find your place at Texas A&M—reach out with any questions and become part of our vibrant community!
+            <p className="text-base lg:text-2xl">
+              Established in 1963, the Texas A&M Chinese Student Association (CSA) is a welcoming community that serves our university through social and cultural Chinese experiences. In CSA, we aim to create lifelong friendships and memories through our general meetings and events. We are excited to help you find your place at Texas A&M!
             </p>
           </div>
-          <iframe className="w-sm h-54 sm:w-lg sm:h-72 md:w-[3000px] md:h-[285px] rounded-xl" src="https://www.youtube.com/embed/V1482ryyZcc?si=sWGN36cdfbvTGZPW" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+          <iframe className="aspect-video sm:w-md md:w-xs lg:w-sm xl:w-md rounded-xl" src="https://www.youtube.com/embed/V1482ryyZcc?si=sWGN36cdfbvTGZPW" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     </div>
   );
