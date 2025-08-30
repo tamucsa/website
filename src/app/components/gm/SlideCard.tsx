d167c4c10c064e0b6842798baba38a97b36e4372
Akhil Kasamsetty <awesomesetty99@gmail.com>
2025-08-29 19:57:20 -0500

    new tab for slides

--- src/app/components/gm/SlideCard.tsx
+++ src/app/components/gm/SlideCard.tsx
@@ -11,7 +11,7 @@ export default function SlideCard({ title, embedLink, slideLink }: SlideCardProp
         <div className="flex flex-col items-center gap-3 w-full">
             <h1 className="font-primary tracking-wider text-2xl lg:text-4xl">{title}</h1>
             <iframe className="rounded-xl w-full lg:w-3xl h-auto" style={{ aspectRatio: "960/569" }} src={embedLink} width="960" height="569" allowFullScreen={true}></iframe>
-            <Link href={slideLink} className="font-primary tracking-wider text-lg lg:text-2xl underline hover:text-primary transition-colors">View Slides</Link>
+            <Link href={slideLink} target="_blank" className="font-primary tracking-wider text-lg lg:text-2xl underline hover:text-primary transition-colors">View Slides</Link>
         </div>
     );
 }
