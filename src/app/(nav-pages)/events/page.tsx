508d36aee0a718b00a229135ebb3938dccf26394
Akhil Kasamsetty <awesomesetty99@gmail.com>
2025-09-04 17:10:45 -0500

    updated event page margins

--- src/app/(nav-pages)/events/page.tsx
+++ src/app/(nav-pages)/events/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const EventsPage = () => {
     return (
-        <div className='flex flex-col items-center mt-10 gap-7 pb-5 md:mt-15 md:gap-10 md:pb-10'>
+        <div className='flex flex-col items-center mt-10 gap-7 pb-5 md:mt-15 md:gap-10 md:pb-15'>
             <h1 className='container text-center font-primary tracking-wider text-3xl md:text-4xl'>Upcoming Events!</h1>
             <div className="flex flex-col xl:flex-row gap-3 xl:gap-10">
                 {/* <LinkButton href="https://docs.google.com/forms/d/e/1FAIpQLSdGs9iM-fFD9oBaup3tKjEhoMmOpkeVV_hbwBqGoUDV5bApEQ/viewform" newTab={true}
