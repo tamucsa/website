336b9684999daa09b10d34606dabd02dec92dab2
Akhil Kasamsetty <awesomesetty99@gmail.com>
2025-09-11 23:56:01 -0500

    updated GM slide format & album storage, added gm#1 info & forms

--- src/app/(nav-pages)/gm/page.tsx
+++ src/app/(nav-pages)/gm/page.tsx
@@ -1,4 +1,5 @@
 import SlideCard from "@/app/components/gm/SlideCard";
+import { GMSlideshowList } from "@/content/2025-2026/slides";
 
 import { Metadata } from "next";
 import Link from "next/link";
@@ -8,9 +9,11 @@ const GMPage = () => {
         <div className='flex flex-col items-center gap-5 sm:gap-10 md:gap-10 mx-10 md:pb-10'>
             <h2 className='container text-center font-secondary tracking-wide text-xl sm:text-3xl lg:text-4xl/12 mt-5 md:mt-15'>
                 Come join us at our General Meeting <b>every other Thursday</b> in <b>PSYC 338</b> from <b>7-8 PM</b>! 
-                Check out <Link href='/events' className="underline hover:text-primary transition-colors">Events</Link> to see the calendar for more accurate dates!
+                Check out the calendar on <Link href='/events' className="underline hover:text-primary transition-colors">Events</Link> for more accurate dates!
             </h2>
-            <SlideCard title="Informational!" embedLink="https://docs.google.com/presentation/d/e/2PACX-1vQXI_ySEMMJ-6236kV-ZDf_qTaGVS_fcOM3VSErrSCNrFCeVz1Z8MghALy6EQo7ecOM84qbdly7dH1l/pubembed?start=true&loop=false&delayms=5000" slideLink="https://docs.google.com/presentation/d/e/2PACX-1vQXI_ySEMMJ-6236kV-ZDf_qTaGVS_fcOM3VSErrSCNrFCeVz1Z8MghALy6EQo7ecOM84qbdly7dH1l/pub?start=true&loop=false&delayms=5000" />
+            {GMSlideshowList.toReversed().map((album) => (
+                <SlideCard key={album.title} title={album.title} baseUrl={album.baseUrl} />
+            ))}
             {/* Image Placeholders */}
             {/* <div className="flex flex-col md:flex-row gap-10 sm:gap-15 mb-10">
                 <div className="relative md:w-xs lg:w-md hover:scale-102 brightness-90 transition-transform duration-300">
--- src/app/(nav-pages)/membership/page.tsx
+++ src/app/(nav-pages)/membership/page.tsx
@@ -7,10 +7,16 @@ const MembershipPage = () => {
     return (
         <div className='flex flex-col items-center gap-5 md:gap-15 mb-10 mx-13 md:mx-13'>
             <h1 className='container text-center font-primary tracking-wider text-3xl md:text-4xl mt-10 md:mt-15'>How to Join CSA!</h1>
-            {/* <LinkButton href="https://forms.gle/De3cQX2uC17khfsB6" newTab={true}
-                className="w-full md:w-lg text-nowrap text-xl md:text-2xl"
-                text="CSA Interest Form (2025-2026)"
-            /> */}
+            <div className="flex flex-col lg:flex-row gap-3 lg:gap-10">
+                <LinkButton href="https://docs.google.com/forms/d/e/1FAIpQLSecQc7JYAfMWQ634FLjHDyj6NEhga-4MrU_PCJQSQpEaJe-rw/viewform" newTab={true}
+                    className="w-full md:w-md text-nowrap text-xl md:text-2xl"
+                    text="CSA Media Team Application! (Due 9/25)"
+                />
+                <LinkButton href="https://tx.ag/csaintern25" newTab={true}
+                    className="w-full md:w-md text-nowrap text-xl md:text-2xl"
+                    text="CSA Intern Application! (Due 10/12)"
+                />
+            </div>
             <div className='flex flex-col lg:flex-row items-center justify-center gap-5 md:gap-10'>
                 {/* Left Text */}
                 <div className="flex flex-col items-center justify-center font-secondary text-md md:text-2xl/10 tracking-wide gap-5">
@@ -31,8 +37,8 @@ const MembershipPage = () => {
                         */}
                     </div>
                     <LinkButton href="https://tx.ag/csamembership" newTab={true}
-                        className="w-full text-nowrap text-xl md:text-2xl"
-                        text="CSA Membership Form!"
+                        className="w-full text-xl md:text-2xl"
+                        text="CSA Membership Form! (Due 9/19)"
                     />
                 </div>
             </div>
