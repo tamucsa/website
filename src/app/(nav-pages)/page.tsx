f37168a5ea66dceb67fcc80813548091bd2b520c
Andrew Zhang <113492071+alockinalock@users.noreply.github.com>
2026-03-11 21:42:33 -0500

    restructure CSS

--- src/app/(nav-pages)/page.tsx
+++ src/app/(nav-pages)/page.tsx
@@ -4,18 +4,18 @@ import { Metadata } from "next";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center md:gap-10 md:m-7">
+    <div className="home-container">
         {/* <UnderConstruction /> */}
         <HomeCard className=""/>
-        <div className="flex flex-col md:flex-row items-center md:items-start m-7 md:m-0 gap-10 md:gap-30">
+        <div className="home-content">
           {/* What is CSA? */}
-          <div className="flex flex-col gap-3 lg:gap-5">
-            <h2 className="text-2xl lg:text-4xl font-primary"><u>What is CSA?</u></h2>
-            <p className="text-base lg:text-2xl">
+          <div className="home-description-section">
+            <h2 className="home-title"><u>What is CSA?</u></h2>
+            <p className="home-description">
               Established in 1963, the Texas A&M Chinese Student Association (CSA) is a welcoming community that serves our university through social and cultural Chinese experiences. In CSA, we aim to create lifelong friendships and memories through our general meetings and events. We are excited to help you find your place at Texas A&M!
             </p>
           </div>
-          <iframe className="aspect-video sm:w-md md:w-xs lg:w-sm xl:w-md rounded-xl" src="https://www.youtube.com/embed/V1482ryyZcc?si=sWGN36cdfbvTGZPW" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+          <iframe className="home-video" src="https://www.youtube.com/embed/V1482ryyZcc?si=sWGN36cdfbvTGZPW" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     </div>
   );
